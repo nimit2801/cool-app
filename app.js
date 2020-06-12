5e796356c632d4fe70488be9495f8d51df93fc69
nimit1428 <mnimitsavant@outlook.com>
2020-06-13 00:17:04 +0530

    converted into express app

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const http = require('http');
-const port = process.env.PORT || 3000
+const http = require("http");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>HEllo sucKerS!</h1>");
 });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+app.listen(port, () => {
+  console.log("running!");
 });
